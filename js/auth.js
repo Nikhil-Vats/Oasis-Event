2692fced37d11ec3e1620879d5a001cc59fdf5ea
NikhilPhalange <nikvats5499@gmail.com>
2018-10-17 23:51:32 +0530

    Added login

--- js/auth.js
+++ js/auth.js
@@ -6,6 +6,7 @@ firebase.auth().signInWithRedirect(provider);
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
+      console.log(user);
     } else {
       // No user is signed in.
     }
