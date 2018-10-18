3fbf7064316e0fe277388868a9eba49ba7b90d74
NikhilPhalange <nikvats5499@gmail.com>
2018-10-18 22:21:56 +0530

    Added get initial Info

--- js/auth.js
+++ js/auth.js
@@ -7,35 +7,80 @@ function signIn() {
 var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().signInWithRedirect(provider);
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      console.log(user);
-      user1 = user;
-      alert('Hi this is in if user');
-      redirect();
-    } else {
-      // No user is signed in.
-    }
-  });
+// firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//       // User is signed in.
+//       console.log(user);
+//       user1 = user;
+//       alert('Hi this is in if user');
+//       redirect();
+//     } else {
+//       // No user is signed in.
+//     }
+//   });
 
+//   var user = firebase.auth().currentUser;
+
+// if (user != null) {
+//   alert('Hi this is in if user != null');
+//   redirect();
+//   user.providerData.forEach(function (profile) {
+//     name = profile.displayName;
+//     alert('Hi this is in in if user != null');
+//     redirect();
+//     console.log("Sign-in provider: " + profile.providerId);
+//     console.log("  Provider-specific UID: " + profile.uid);
+//     console.log("  Name: " + profile.displayName);
+//     console.log("  Email: " + profile.email);
+//     console.log("  Photo URL: " + profile.photoURL);
+//   });
+// }
+}
+
+firebase.auth().getRedirectResult().then(function (result) {
   var user = firebase.auth().currentUser;
-
-if (user != null) {
-  alert('Hi this is in if user != null');
+  name = "no name";
   redirect();
-  user.providerData.forEach(function (profile) {
-    name = profile.displayName;
-    alert('Hi this is in in if user != null');
+  if (user != null) {
+    alert('Hi this is in if user != null');
     redirect();
-    console.log("Sign-in provider: " + profile.providerId);
-    console.log("  Provider-specific UID: " + profile.uid);
-    console.log("  Name: " + profile.displayName);
-    console.log("  Email: " + profile.email);
-    console.log("  Photo URL: " + profile.photoURL);
-  });
-}
-}
+    user.providerData.forEach(function (profile) {
+      name = profile.displayName;
+      alert('Hi this is in in if user != null');
+      redirect();
+      console.log("Sign-in provider: " + profile.providerId);
+      console.log("  Provider-specific UID: " + profile.uid);
+      console.log("  Name: " + profile.displayName);
+      console.log("  Email: " + profile.email);
+      console.log("  Photo URL: " + profile.photoURL);
+    });
+  }
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  if (result.credential) {
+      var token = result.credential.accessToken;
+      console.log('token ' + token);
+  }
+  // The signed-in user info.
+  var user2 = result.user;
+  console.log('user ' + user2);
+  // if success redirect to
+  $state.go('maps-fullwidth');
+
+  // ...
+}).catch(function (error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  console.log(errorCode);
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  console.log(errorMessage);
+  var email = error.email;
+  console.log(email);
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(credential);
+  // ...
+});
 
 function redirect() {
   window.location.href = 'https://nikhilphalange.github.io/Oasis-Event/round1.html';
