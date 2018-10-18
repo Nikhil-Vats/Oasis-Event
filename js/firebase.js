b6f980dc53dcb7fa9dd5004a29c56a343ef235bf
NikhilPhalange <nikvats5499@gmail.com>
2018-10-18 23:07:02 +0530

    Added get initial Info

--- js/firebase.js
+++ js/firebase.js
@@ -2,7 +2,7 @@ var app_fireBase = {};
 
 var config = {
     apiKey: "AIzaSyDz7s1lXGSVhAEj8TIQSsEjeatPO5h9e2c",
-    authDomain: "oasis-event.firebaseapp.com",
+    authDomain: "https://nikhilphalange.github.io/Oasis-Event/round1.html",
     databaseURL: "https://oasis-event.firebaseio.com",
     projectId: "oasis-event",
     storageBucket: "oasis-event.appspot.com",
