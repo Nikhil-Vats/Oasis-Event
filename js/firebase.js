c55701521e62b7d28cc3ba3485c83a2f6186d20c
NikhilPhalange <nikvats5499@gmail.com>
2018-10-24 04:15:35 +0530

    Fixed bugs

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,12 @@
+let vh = window.innerHeight * 0.01;
+document.getElementById('loading').style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.getElementById('loading').setProperty('--vh', `${vh}px`);
+});
+
 var app_fireBase = {};
 
 var config = {
